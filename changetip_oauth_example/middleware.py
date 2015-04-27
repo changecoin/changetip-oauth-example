14d8a5faa2b4fb6d46744954d587c8af970e9383
honestbleeps <steve@openingbands.com>
2015-04-27 18:13:12 -0500

    add comment

--- changetip_oauth_example/middleware.py
+++ changetip_oauth_example/middleware.py
@@ -7,6 +7,8 @@ from social import exceptions as social_exceptions
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
         if hasattr(social_exceptions, 'AuthCanceled'):
+            # Note: You'll probably want to update this for your own specific application
+            # and do something more than just redirect to the homepage.
             return redirect('/')
         else:
             raise exception
